51b731a62693ef09256e94fb6e1b8c0a17534f78
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2023-01-19 18:58:45 +0100

    when searching -> displays movies in main

--- src/js/search-line.js
+++ src/js/search-line.js
@@ -1,6 +1,8 @@
 import MoviesApiService from "./api-service";
+import templateFunction from '../templates/list-movies.hbs';
 
 const moviesApiService = new MoviesApiService();
+const refMain = document.querySelector('main');
 
 export default function onSearchLine(event) {
   event.preventDefault();
@@ -9,6 +11,8 @@ export default function onSearchLine(event) {
   } = event.currentTarget;
     console.log(search.value);
     
-    moviesApiService.getSearchMovies(search.value);
+    moviesApiService.getSearchMovies(search.value).then(movies => {
+        refMain.innerHTML = templateFunction(movies)
+    });
 }
 
